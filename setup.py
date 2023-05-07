bd1686c952f1d5a77d4a00f84a22c87649bd9bfd
BoudjemaaAKHAM <akhamboudjemaa@gmail.com>
2023-05-07 12:47:19 +0200

    update requirements.txt and setup.py

--- setup.py
+++ setup.py
@@ -1,11 +1,22 @@
 from setuptools import setup, find_packages
 
+with open('requirements.txt') as f:
+    requirements = f.read().splitlines()
+
+with open("webshopapi/__init__.py") as f:
+    for line in f:
+        if line.startswith("__version__"):
+            version = line.strip().split("=")[-1].strip(" '\"")
+            break
+    else:
+        raise RuntimeError("Unable to find version string.")
+
 setup(
     name='webshopapi',
-    version='1.0.0',
-    description='',
-    author='',
+    version=version,
+    description='Webshop API',
+    author='EPSI Grenoble I1 Dev Groupe 3',
     packages=find_packages(),
     include_package_data=True,
-    install_requires=['fastapi', 'uvicorn', 'requests', 'pydantic']
+    install_requires=requirements
 )
